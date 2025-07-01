eb33ddc19102779f13c04b5d022ccb7f89b62758
Amanda Shih <pandafulmanda@gmail.com>
2025-06-30 19:39:23 -0500

    faqs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
-const postsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
-  }),
-});
+// const postsCollection = defineCollection({
+//   type: "content",
+//   schema: z.object({
+//     title: z.string(),
+//     pubDate: z.date(),
+//     description: z.string(),
+//     author: z.string(),
+//     image: z.object({
+//       url: z.string(),
+//       alt: z.string(),
+//     }),
+//     tags: z.array(z.string()),
+//   }),
+// });
 
 const stepsCollection = defineCollection({
   type: "content",
@@ -27,8 +27,17 @@ const stepsCollection = defineCollection({
   }),
 });
 
+const faqsCollection = defineCollection({
+  type: "content",
+  schema: () => z.object({
+    title: z.string(),
+    order: z.number(),
+  }),
+})
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  posts: postsCollection,
+  // posts: postsCollection,
   steps: stepsCollection,
+  faqs: faqsCollection,
 };
