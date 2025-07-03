27e565a072c32706054fb61846d04e9ed877d4bb
Amanda Shih <pandafulmanda@gmail.com>
2025-07-03 16:33:46 -0500

    fix nav

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,34 @@
+import scrollama from "scrollama";
+console.log('wahts up')
+
 document.addEventListener("astro:page-load", () => {
   document.querySelector(".hamburger")?.addEventListener("click", () => {
     document.querySelector(".nav-links")?.classList.toggle("expanded");
   });
+
+  console.log('HELLO?')
+
+  const headerEl = document.querySelector('header')
+  const scroller = scrollama();
+
+  console.log({headerEl})
+  
+  if (headerEl) {
+    headerEl.dataset.scrollDirection = 'up'
+    console.log('is this happerning?')
+    scroller
+      .setup({
+        step: document.querySelectorAll('main'),
+        offset: 0,
+      })
+      .onStepEnter((response) => {
+        console.log('hello?', response)
+        headerEl.dataset.scrollDirection = response.direction
+      })
+      .onStepExit((response) => {
+        console.log('goodbye?', response)
+        headerEl.dataset.scrollDirection = response.direction
+      });
+  }
 });
+
