808d42ca542605e029071198eddb5e9c18e8bf28
Amanda Shih <pandafulmanda@gmail.com>
2025-07-30 15:17:29 -0500

    fix base url for new custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mokshadata.github.io',
-  base: 'linc-demo',
+  site: 'https://beta.fortbendlinc.org',
+  trailingSlash: 'always',
   integrations: [react(), icon(), mdx(), sitemap()],
   image: {
     domains: ["astro.build"],
